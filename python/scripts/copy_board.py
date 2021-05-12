3e14bd39d78e3e2ca9a4d017b0f16fa90fd8b672
David Chaiken <dchaiken@pinterest.com>
2021-05-12 14:59:29 -0700

    python: implement all option for copy_board script

--- python/scripts/copy_board.py
+++ python/scripts/copy_board.py
@@ -20,7 +20,7 @@ from oauth_scope import Scope
 
 def main(argv=[]):
     parser = argparse.ArgumentParser(description='Copy a Board')
-    parser.add_argument('-b', '--board-id', required=True, help='source board identifier')
+    parser.add_argument('-b', '--board-id', help='source board identifier')
     parser.add_argument('-n', '--name', help='target board name')
     parser.add_argument('-s', '--source-access-token', help='source access token name')
     parser.add_argument('-t', '--target-access-token', help='target access token name')
@@ -44,12 +44,21 @@ def main(argv=[]):
             exit(1)
 
     if args.all_boards and args.name:
+        parser.print_usage()
         print('the name and all options are mutually exclusive')
         exit(1)
 
-    if args.all_boards and args.board_id:
-        print('the board-id and all options are mutually exclusive')
-        exit(1)
+    if args.all_boards:
+        if args.board_id:
+            parser.print_usage()
+            print('the board-id and all options are mutually exclusive')
+            exit(1)
+    else:
+        if not args.board_id:
+            parser.print_usage()
+            print('board-id is a required argument when not copying all boards')
+            exit(1)
+
 
     # get configuration from defaults and/or the environment
     api_config = ApiConfig()
@@ -58,15 +67,16 @@ def main(argv=[]):
     # imports that depend on the version of the API
     from board import Board
     from pin import Pin
+    from user import User
 
     def copy_pin(pin, pin_data, target_board_id, target_section_id=None):
         try:
             if pin_data['type'] == 'pin':
                 print('source pin:')
                 Pin.print_summary(pin_data)
-                new_pin_data = pin.create(pin_data, target_board_id, target_section_id)
-                print('new pin:')
-                Pin.print_summary(new_pin_data)
+                target_pin_data = pin.create(pin_data, target_board_id, target_section_id)
+                print('target pin:')
+                Pin.print_summary(target_pin_data)
             else:
                 print("skipping pin because type is not 'pin'")
                 Pin.print_summary(pin_data)
@@ -90,53 +100,62 @@ def main(argv=[]):
 
     target_pin = Pin(None, api_config, target_token) # pin_id set by create
 
-    board = Board(args.board_id, api_config, source_token)
-    board_data = board.get()
-    print('source board:')
-    Board.print_summary(board_data)
+    if args.all_boards: # copy all boards for the source user
+        user_me = User('me', api_config, source_token)
+        user_me_data = user_me.get()
+        boards = user_me.get_boards(user_me_data)
+        source_board = Board(None, api_config, source_token) # board_id set in loop below
+    else: # copy just the board designated by board_id
+        source_board = Board(args.board_id, api_config, source_token)
+        source_board_data = source_board.get()
+        # use different name, which is mandatory when using a single access token
+        if args.name:
+            print('setting target board name to "' + args.name + '"')
+            source_board_data['name'] = args.name
+        boards = [source_board_data]
 
-    # option: use different name, which should be mandatory when using
-    # a single access token
-    if args.name:
-        print('setting target board name to "' + args.name + '"')
-        board_data['name'] = args.name
+    for source_board_data in boards:
 
-    new_board = Board(None, api_config, target_token) # board_id set by create
-    if args.dry_run:
-        print('dry-run: skipping attempt to create board:')
-        Board.print_summary(board_data)
-    else:
-        new_board_data = new_board.create(board_data)
-        print('new board:')
-        Board.print_summary(new_board_data)
+        print('source board:')
+        Board.print_summary(source_board_data)
+        source_board.board_id = source_board_data['id']
 
-    # copy board pins
-    for pin_data in board.get_pins():
+        target_board = Board(None, api_config, target_token) # board_id set by create
         if args.dry_run:
-            print('dry-run: skipping attempt to create board pin:')
-            Pin.print_summary(pin_data)
+            print('dry-run: skipping attempt to create board:')
+            Board.print_summary(source_board_data)
         else:
-            copy_pin(target_pin, pin_data, new_board_data['id'])
+            target_board_data = target_board.create(source_board_data)
+            print('target board:')
+            Board.print_summary(target_board_data)
 
-    sections_iterator = board.get_sections()
-    for idx, section_data in enumerate(sections_iterator):
-        if args.dry_run:
-            print('dry-run: skipping attempt to create board section:')
-            Board.print_section(section_data)
-        else:
-            print(f'source section #{idx}:')
-            Board.print_section(section_data)
-            new_section_data = new_board.create_section(section_data)
-            print(f'new section #{idx}:')
-            Board.print_section(new_section_data)
-        
-        # copy board section pins
-        for pin_data in board.get_section_pins(section_data['id']):
+        # copy board pins
+        for pin_data in source_board.get_pins():
             if args.dry_run:
-                print('dry-run: skipping attempt to create board section pin:')
+                print('dry-run: skipping attempt to create board pin:')
                 Pin.print_summary(pin_data)
             else:
-                copy_pin(target_pin, pin_data, new_board_data['id'], new_section_data['id'])
+                copy_pin(target_pin, pin_data, target_board_data['id'])
+
+        sections_iterator = source_board.get_sections()
+        for idx, section_data in enumerate(sections_iterator):
+            if args.dry_run:
+                print('dry-run: skipping attempt to create board section:')
+                Board.print_section(section_data)
+            else:
+                print(f'source section #{idx}:')
+                Board.print_section(section_data)
+                target_section_data = target_board.create_section(section_data)
+                print(f'target section #{idx}:')
+                Board.print_section(target_section_data)
+
+            # copy board section pins
+            for pin_data in source_board.get_section_pins(section_data['id']):
+                if args.dry_run:
+                    print('dry-run: skipping attempt to create board section pin:')
+                    Pin.print_summary(pin_data)
+                else:
+                    copy_pin(target_pin, pin_data, target_board_data['id'], target_section_data['id'])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
