ac66e8ceb6f59b7e359e45b7b4055828c190df39
David Chaiken <dchaiken@pinterest.com>
2021-05-25 10:49:07 -0700

    python: more documentation for get_access_token and copy_board

--- python/scripts/copy_board.py
+++ python/scripts/copy_board.py
@@ -19,6 +19,32 @@ from access_token import AccessToken
 from oauth_scope import Scope
 
 def main(argv=[]):
+    """
+    This script is intended primarily for developers who need to create a test copy
+    of a board or all of the boards in an account. For each board, it copies the
+    board itself, all of the pins on the board, all of the board sections, and
+    all of the pins on each section. There are three intended use cases,
+    each with its own combination of arguments. All three use cases require at least
+    one access token, which may be created using the get_access_token.py script. All
+    three also require a source board identifier, which can be discovered using the
+    get_user_boards.py script.
+    1. Copy a board within a single account. In this case, it is possible to use a
+       single access token with the default name. Since each board in an account
+       requires a unique name, the target board name must be specified. Here's an
+       example of the command-line arguments for this use case:
+         ./copy_board.py -b <board-id> -n 'test board 001'
+    2. Copy a board from one account to another, which requires specifying one
+       access token for the source account and one for the target account. See
+       the comments at the top of the get_access_tokeh.py script for information
+       about where access tokens are stored.
+       For example:
+         ./copy_board.py -b <board-id> -s source_account_token.json -t target_account_token.json
+    3. Copy all of the boards from one account to another. This use case is designed
+       to be used by developers to create a test account. This use case creates a lot of data,
+       so it is advisable to use the dry-run argument to verify the pins and boards to be copied.
+         ./copy_board.py --dry-run --all -s source_account_token.json -t target_account_token.json
+         ./copy_board.py --all -s source_account_token.json -t target_account_token.json
+    """
     parser = argparse.ArgumentParser(description='Copy a Board')
     parser.add_argument('-b', '--board-id', help='source board identifier')
     parser.add_argument('-n', '--name', help='target board name')
@@ -28,6 +54,8 @@ def main(argv=[]):
     parser.add_argument('--dry-run', action='store_true', help='print changes but do not execute them')
     args = parser.parse_args(argv)
 
+    # Check the combinations of arguments. The comment at the top of this function
+    # describes the intended use cases.
     if args.target_access_token:
         if not args.source_access_token:
             parser.print_usage()
@@ -69,9 +97,12 @@ def main(argv=[]):
     from pin import Pin
     from user import User
 
+    # helper function to copy a pin
     def copy_pin(pin, pin_data, target_board_id, target_section_id=None):
         try:
             pintype = pin_data.get('type')
+            # Sometimes the board list operation will generate entities (e.g. "more ideas"
+            # tiles) that resemble pins but can not be copied.
             if not pintype or pintype == 'pin':
                 print('source pin:')
                 Pin.print_summary(pin_data)
@@ -84,22 +115,23 @@ def main(argv=[]):
         except SpamException:
             print('skipping pin because of spam exception')
 
-    # Note: The same API configuration is used with both the
-    # source and target access tokens.
-
+    # Note: The same API configuration is used with both the source and target access tokens.
     if args.source_access_token:
         source_token = AccessToken(api_config, name=args.source_access_token)
     else:
         source_token = AccessToken(api_config)
     source_token.fetch(scopes=[Scope.READ_PINS, Scope.READ_BOARDS]) # get the token
 
+    # Default to use the source token (same account) if the target token is not specified.
     if args.target_access_token:
         target_token = AccessToken(api_config, name=args.target_access_token)
-        target_token.fetch(scopes=[Scope.WRITE_PINS,Scope.WRITE_BOARDS]) # TODO: need write scope?
+        target_token.fetch(scopes=[Scope.WRITE_PINS,Scope.WRITE_BOARDS])
     else:
         target_token = source_token # already have the token
 
-    target_pin = Pin(None, api_config, target_token) # pin_id set by create
+    # This Pin object is reusable. The pin_id attribute is set when the
+    # create method is called successfully.
+    target_pin = Pin(None, api_config, target_token)
 
     if args.all_boards: # copy all boards for the source user
         user_me = User('me', api_config, source_token)
@@ -121,7 +153,9 @@ def main(argv=[]):
         Board.print_summary(source_board_data)
         source_board.board_id = source_board_data['id']
 
-        target_board = Board(None, api_config, target_token) # board_id set by create
+        # This Board object is reusable. The board_id is set when the
+        # create method is called successfully.
+        target_board = Board(None, api_config, target_token)
         if args.dry_run:
             print('dry-run: skipping attempt to create board:')
             Board.print_summary(source_board_data)
@@ -138,6 +172,7 @@ def main(argv=[]):
             else:
                 copy_pin(target_pin, pin_data, target_board_data['id'])
 
+        # get and copy board sections
         sections_iterator = source_board.get_sections()
         for idx, section_data in enumerate(sections_iterator):
             if args.dry_run:
@@ -158,5 +193,7 @@ def main(argv=[]):
                 else:
                     copy_pin(target_pin, pin_data, target_board_data['id'], target_section_data['id'])
 
+# If this script is being called from the command line, call the main function
+# with the arguments. The other use case is to call main() from an integration test.
 if __name__ == '__main__':
     main(sys.argv[1:])
