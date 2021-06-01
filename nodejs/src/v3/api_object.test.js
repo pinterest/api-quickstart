0aefb2d69573e0f246cd8923204f37230c588f91
David Chaiken <dchaiken@pinterest.com>
2021-06-01 12:04:01 -0700

    nodejs: add access_token test, update api_object test

--- nodejs/src/v3/api_object.test.js
+++ nodejs/src/v3/api_object.test.js
@@ -4,18 +4,14 @@ import got from 'got'
 jest.mock('got');
 
 describe('v3 api_object tests', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  test('v3 api_object oauth', async () => {
+    const mock_api_config = jest.fn();
+    mock_api_config.api_uri = 'test_uri';
+    mock_api_config.verbosity = 2;
 
-  test('v3 api_object request_data', async () => {
-    const api_config = jest.fn();
-    api_config.api_uri = 'test_uri';
-    api_config.verbosity = 2;
-
-    const access_token = jest.fn();
-    access_token.header = jest.fn();
-    access_token.header.mockReturnValueOnce('test_headers');
+    const mock_access_token = jest.fn();
+    mock_access_token.header = jest.fn();
+    mock_access_token.header.mockReturnValueOnce('test_headers');
 
     got.get.mockResolvedValueOnce({'body': {'data': 'test_response_data'},
                                    'statusCode': 200
@@ -24,7 +20,7 @@ describe('v3 api_object tests', () => {
     // check output
     console.log = jest.fn();
     
-    const api_object = new ApiObject(api_config, access_token);
+    const api_object = new ApiObject(mock_api_config, mock_access_token);
     const response = await api_object.request_data('/test_path');
     expect(response).toEqual('test_response_data');
     expect(got.get.mock.calls[0][0]).toEqual('test_uri/test_path');
