3e6bfb04cf046b2d8942a5973402b22148f369bd
David Chaiken <dchaiken@pinterest.com>
2021-07-07 11:56:09 -0700

    repo moved to pinterest org

--- nodejs/babel.config.cjs
+++
@@ -1,6 +0,0 @@
-// Jest required Babel to handle the modern module structure used by this nodejs code.
-// This file has the standard, minimal configuration for using Babel with Jest.
-// For this configuration to work, Need to run: npm install --save-dev @babel/preset-env
-module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-};
