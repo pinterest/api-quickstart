3c5a53f957f62f7a4fef2e11fac2165828999b87
David Chaiken <dchaiken@pinterest.com>
2022-11-01 13:23:34 -0700

    nodejs: v5 get delivery metrics working and tested

--- nodejs/src/delivery_metrics.js
+++ nodejs/src/delivery_metrics.js
@@ -1,13 +1,30 @@
-/**
- * This module will provide access to delivery metrics when it becomes available.
- */
+import { ApiObject } from './api_object.js';
 
 // Use this class to get and to print all of the available
 // advertising delivery metrics.
-//   https://developers.pinterest.com/docs/api/v5/#operation/delivery_metrics/get
-export class DeliveryMetrics {
-  // The full list of all available delivery metrics will be available in v5 soon.
+export class DeliveryMetrics extends ApiObject {
+  // Get the full list of all available delivery metrics.
+  // This call is not used much in day-to-day API code, but is a useful endpoint
+  // for learning about the metrics.
+  // https://developers.pinterest.com/docs/api/v5/#operation/delivery_metrics/get
   async get() {
-    throw new Error('Metric definitions are not available in API version v5.');
+    return (await this.request_data('/v5/resources/delivery_metrics')).items;
+  }
+
+  summary(delivery_metric) {
+    return `${delivery_metric.name}: ${delivery_metric.definition}`;
+  }
+
+  // Print summary of a single metric.
+  print(delivery_metric) {
+    console.log(this.summary(delivery_metric));
+  }
+
+  // Print summary of data returned by get().
+  print_all(delivery_metrics) {
+    console.log('Available delivery metrics:');
+    for (const [idx, metric] of delivery_metrics.entries()) {
+      console.log(`[${idx + 1}] ${this.summary(metric)}`);
+    }
   }
 }
--- nodejs/src/delivery_metrics.test.js
+++ nodejs/src/delivery_metrics.test.js
@@ -1,4 +1,7 @@
+import { ApiObject } from './api_object.js';
 import { DeliveryMetrics } from './delivery_metrics.js';
+
+jest.mock('./api_object');
 
 describe('delivery_metrics tests', () => {
   afterEach(() => {
@@ -6,12 +9,27 @@ describe('delivery_metrics tests', () => {
   });
 
   test('delivery metrics', async() => {
-    const test_dm = new DeliveryMetrics();
+    const test_dm = new DeliveryMetrics('test_api_config', 'test_access_token');
+    expect(ApiObject.mock.instances.length).toBe(1);
+    expect(ApiObject.mock.calls[0]).toEqual(['test_api_config', 'test_access_token']);
 
-    await expect(async() => {
-      await test_dm.get();
-    }).rejects.toThrowError(
-      new Error('Metric definitions are not available in API version v5.')
-    );
+    const mock_request_data = jest.spyOn(ApiObject.prototype, 'request_data');
+    mock_request_data.mockResolvedValueOnce({
+      items: [
+        { name: 'metric1', definition: 'description 1' },
+        { name: 'metric2', definition: 'description 2' }
+      ]
+    });
+    const metrics = await test_dm.get();
+
+    expect(test_dm.summary(metrics[1])).toEqual('metric2: description 2');
+
+    console.log = jest.fn(); // test output
+    test_dm.print_all(metrics);
+    expect(console.log.mock.calls).toEqual([
+      ['Available delivery metrics:'],
+      ['[1] metric1: description 1'],
+      ['[2] metric2: description 2']
+    ]);
   });
 });
